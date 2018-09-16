586aa31e1ba9aec2f8998f7b8d4d04c54522e8d1
Alexander Balan <fantom141141@gmail.com>
2018-09-16 22:02:00 +0300

    Implements brackets checker

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    if (str.length <= 1) {
+        return false
+    }
+
+    let matchingOpeningBracket, ch;
+    let stack = [];
+
+    let openingBrackets = ['[', '{', '(', '.|'];
+    let closingBrackets = [']', '}', ')', '|.'];
+
+    for (let i = 0; i < str.length; i++) {
+        ch = str[i];
+
+        if (ch === '|') {
+            if (!stack.length || closingBrackets.includes(stack[stack.length - 1]) || stack[stack.length - 1] !== '.|') {
+                ch = '.|';
+            } else {
+                ch = '|.';
+            }
+        }
+
+        if (closingBrackets.includes(ch)) {
+            matchingOpeningBracket = openingBrackets[closingBrackets.indexOf(ch)];
+            if (!stack.length || (stack.pop() !== matchingOpeningBracket)) {
+                return false
+            }
+        } else {
+            stack.push(ch)
+        }
+    }
+
+    return (stack.length === 0);
 }
